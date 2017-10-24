a0389e578cc17df5dd26cee87f6bd6b873a9842c
u1139622 <u1139622@eae-d-057>
2017-10-24 14:51:34 -0600

    Updated script for multiple text objects in whiteboard - individual dyslexic words

--- Assets/_Scripts/gazeTrigger.cs
+++ Assets/_Scripts/gazeTrigger.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class gazeTrigger : MonoBehaviour {
 
 	// Use this for initialization
-	public TextMesh _text;
+	public TextMesh _activeMesh;
 	public RaycastHit hit;
 	int[] easy = {0, 2, 1, 5, 4, 3, 6, 7, 9, 8, 10, 12, 11, 13, 14, 15, 17, 16, 18, 19, 22, 20, 21, 23, 24, 25};
 	int[] medium = { 0, 2, 3, 1, 5, 4, 6, 8, 10, 7, 9, 12, 11, 13, 14, 17, 15, 16, 18, 19, 21, 20, 23, 24, 22, 25 };
@@ -19,23 +19,29 @@ public class gazeTrigger : MonoBehaviour {
 	char[] similar6 = { 'T', 'I', 'L' };
 	char[] similar7 = { 'V', 'A'};
 
+	int _tempIndex = 0;
+
 	GameObject _whiteboard;
-	TextMesh _textMesh;
-	string original;
+	TextMesh[] _textMesh = new TextMesh[4];
+	string[] original = new string[4];
 	string copy;
 	string word ="";
 	float x, y, z;
 	float jumbleTime = 0.5f;
 	float flipTime = 2f;
 	string difficulty;
-	bool makeRandom;
+	bool[] makeRandom = {true, true, true, true};
+	string[] tag = { "board0", "board1", "board2", "board3" };
+	int _activeIndex;
 
 	void Start () {
 		difficulty = "easy";
 		_whiteboard = GameObject.FindWithTag ("board");
-		_textMesh = _whiteboard.GetComponentInChildren<TextMesh>();
-		original = _textMesh.text;
-		makeRandom = true;
+		_textMesh = _whiteboard.GetComponentsInChildren<TextMesh>(true);
+		for (int i = 0; i < _textMesh.Length; i++) {
+			original [i] = _textMesh [i].text;
+		}
+
 	}
 	
 	// Update is called once per frame
@@ -43,42 +49,58 @@ public class gazeTrigger : MonoBehaviour {
 	{
 		Time deltaTime;
 		string s = "";
-		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		int i = 0;
+		Vector3 fwd = transform.TransformDirection (Vector3.forward)*10;
+		Debug.DrawRay(transform.position, fwd, Color.green);
 		if (Time.time > jumbleTime) {
-			if (makeRandom) {
-				StartRandom ();
-				jumbleTime += 1f;
-			}if (hit.collider && hit.collider.tag == "board") {
-					Debug.Log("STill hit");
+			foreach (TextMesh _currentMesh in _textMesh) {
+				_activeIndex = i;
+				i++;
+				string tag = _currentMesh.tag;
+				//print (tag);
+				if (makeRandom [_activeIndex]) {
+					StartRandom (_currentMesh);
+				}
+				if (hit.collider && hit.collider.tag == _currentMesh.tag) {
+					print ("Still Hit");
 				} else {
-				makeRandom = true;
-                Debug.Log("teest");
-			}
+					makeRandom[_activeIndex] = true;
+				}
 
-		if (Physics.Raycast (transform.position, fwd, out hit)) {
-			if (hit.collider && hit.collider.tag == "board") {
-				Invoke ("CheckCollision", 3);
+				if (Physics.Raycast (transform.position, fwd, out hit)) {
+					if (hit.collider && hit.collider.tag == _currentMesh.tag) {
+						StartCoroutine(CallCollisionCheck(_activeIndex));
+					}
+				}
 			}
+			jumbleTime += 1f;
+			i = 0;
 		}
 	}
+
+	IEnumerator CallCollisionCheck(int _tempIndex)	{
+		yield return new WaitForSeconds(3f);
+		CheckCollision(_tempIndex);
 	}
 
-	void CheckCollision() {
+	void CheckCollision(int i_index) {
+		print (i_index);
 		Vector3 fwd2 = transform.TransformDirection (Vector3.forward);
-		if (Physics.Raycast (transform.position, fwd2, out hit)) {
-			if (hit.collider && hit.collider.tag == "board") {
-				makeRandom = false;
-				_textMesh.text = original;
-				Debug.Log ("After dealy");
+			if (Physics.Raycast (transform.position, fwd2, out hit)) {
+			if (hit.collider && hit.collider.tag == "board"+i_index) {
+				makeRandom[i_index] = false;
+					_textMesh[i_index].text = original[i_index];
+					print ("After dealy");
+				}
 			}
+	
 	}
-	}
-	void StartRandom()	{
-		_text = _textMesh;
-		Debug.Log (_text);
+	void StartRandom(TextMesh i_mesh)	{
+		TextMesh o_text = i_mesh;
+		//print (o_text.text);
 		string word = "";
 		string randomText = "";
-		string s = _text.text;
+		string s = o_text.text;
 		s += " ";
 
 			//Debug.Log (s);
@@ -93,7 +115,7 @@ public class gazeTrigger : MonoBehaviour {
 				}
 			}
 			randomText = SimilarWords (randomText);
-		   _textMesh.text = randomText;		
+			i_mesh.text = randomText;		
 	}
 
 	string RandomText(string word, string difficulty)	{
