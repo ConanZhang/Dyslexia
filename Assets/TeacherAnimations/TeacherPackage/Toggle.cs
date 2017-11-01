79695b56870f7aa0e144ce3311f15bf5969c7c8a
u1139622 <u1139622@eae-d-057>
2017-10-31 18:14:23 -0600

    New classroon texture and students

---
+++ Assets/TeacherAnimations/TeacherPackage/Toggle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour {
+
+    Animator m;
+	float speed = 1f;
+    bool walk;
+    bool macarena;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m = gameObject.GetComponent<Animator>();
+
+        walk = false;
+        macarena = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            walk = true;
+            macarena = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            walk = false;
+            macarena = true;
+        }
+        else
+        {
+            macarena = false;
+            walk = false;
+        }
+
+        if (walk == true)
+        {
+            m.SetBool("walk", true);
+//			speed = speed * Time.deltaTime;
+//			m.transform.position = Vector3.MoveTowards (new Vector3 (m.transform.localPosition.x, m.transform.localPosition.y, m.transform.localPosition.z),
+//				new Vector3 (0f, 0f, 0f), speed);
+				
+        }
+        else
+        {
+            m.SetBool("walk", false);
+        }
+
+        if (macarena == true)
+        {
+            m.SetBool("macarena", true);
+        }
+        else
+        {
+            m.SetBool("macarena", false);
+        }
+    }
+}
---
+++ Assets/_Scripts/Walk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+
+	Transform m;
+	public float speed = 100f;
+	// Use this for initialization
+	void Start () {
+		m = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speed = speed * Time.deltaTime*10;
+		print (m.localPosition);
+//		m.transform.position = Vector3.MoveTowards (new Vector3 (m.transform.localPosition.x, m.transform.localPosition.y, m.transform.localPosition.z),
+//						new Vector3 (0f, 0f, 0f), speed);
+
+		m.localPosition.Set (0f, 0f, 0f);
+		print (m.localPosition);
+		}
+}
