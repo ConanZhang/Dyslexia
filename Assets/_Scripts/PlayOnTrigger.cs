3eae2cf1e33af9cbe800705eeaf2bf85ac270ce5
u1139622 <u1139622@eae-d-057>
2017-11-01 20:39:03 -0600

    Timeline and timeline script

---
+++ Assets/_Scripts/PlayOnTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class PlayOnTrigger : MonoBehaviour {
+
+    // Use this for initialization
+    public PlayableDirector director;
+    bool press = false;
+	void Start () {
+        director = gameObject.GetComponent<PlayableDirector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //director.Pause();
+        print(Time.time);
+        if (Time.time > 14 && ! press)
+        {
+            director.Pause();
+        }
+
+        if (Input.GetKey(KeyCode.Space) && !press)
+        {
+            print("presds");
+            press = true;
+            director.Resume();
+        }
+    }
+
+    void Play()  {
+        director.Play();
+    }
+}
+
