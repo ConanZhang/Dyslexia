2aba5bdc8b500e5b95c807e54aa07ebe0f95499f
conankzhang <u0409453@utah.edu>
2017-11-02 00:34:45 -0600

    Setup title screen in classroom scene

--- Assets/_Scripts/PlayOnTrigger.cs
+++ Assets/_Scripts/PlayOnTrigger.cs
@@ -6,30 +6,26 @@ using UnityEngine;
 public class PlayOnTrigger : MonoBehaviour {
 
     // Use this for initialization
-    public PlayableDirector director;
+    PlayableDirector director;
     bool press = false;
 	void Start () {
         director = gameObject.GetComponent<PlayableDirector>();
+        Pause();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //director.Pause();
-        //print(Time.time);
 
-        if (Input.GetKey(KeyCode.Space))// && !press)
-        {
-            print("presds");
-            press = true;
-            director.Resume();
-        }
+    }
 
-        if (Time.time > 14 && ! press)
-        {
-            director.Pause();
-           // print(director.GetComponent<AudioSource>());
-        }
-        
+    public void Resume()
+    {
+        director.Resume();
+    }
+
+    public void Pause()
+    {
+        director.Pause();
     }
 }
 
